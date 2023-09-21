39d9de1dd7f18aea40929d54c58faa6e31e4b3eb
carlos-9203 <carlosalonso9797@gmail.com>
2023-09-21 16:52:28 +0100

    clase person con funciones y empiezo con main

---
+++ src/software/ulpgc/kata1/Main.java
@@ -0,0 +1,10 @@
+package software.ulpgc.kata1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("args = " + Arrays.toString(args));
+    }
+}
