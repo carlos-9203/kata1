99e19f21e3fadfef9ff7f308f94503af40fe4e6a
carlos-9203 <carlosalonso9797@gmail.com>
2023-09-21 16:54:22 +0100

    main ya creado

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,10 +1,11 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("args = " + Arrays.toString(args));
+        Person person = new Person("Carlos", LocalDate.of(2003,2,9));
+        System.out.println("La persona creada se llama "+ person.getName()+ "con edad de " + person.getYears());
     }
 }
